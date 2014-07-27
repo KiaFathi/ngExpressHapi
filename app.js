93de5d5917671daeadc6a1b68cde40afa9d075c1
Kia Fathi <kiafathi@me.com>
2014-07-26 23:17:20 -0700

    Add basic front-end

---
+++ app.js
@@ -0,0 +1,19 @@
+angular.module('app', [
+])
+.controller('myCTRL', ['$scope', '$http', function($scope, $http){
+  $scope.message = 'Hello HR!';
+
+  //message
+    //will update based on server response message
+    // Hi class thanks for getting this message
+      //GET secretMessage
+    // sexy Post Forest
+      //POST to theTruth, respond with 
+    // Kia why are you so destrucive
+      //Delete to theTruth
+
+  $scope.picture = 'assets/hr.png';
+  //Picture
+    //
+}
+]);
